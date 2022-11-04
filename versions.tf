cc9e28d1bf4e2e0493ca31b73406995063eaa7a4
Michal Tomczuk <michal.tomczuk@zdt.io>
2022-11-04 13:49:39 +0100

    test

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.3.1"
   required_providers {
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = ">= 3.22.0"
-    # }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.22.0"
+    }
     # aws = {
     #   source  = "hashicorp/aws"
     #   version = ">= 4.30.0"
