edea3a1aba9635aa8c55a515b80c79a844b7bfd6
Michal Tomczuk <michal.tomczuk@zdt.io>
2024-04-09 19:43:33 +0200

    77 adopt module template to latest development guide rules (#80)
    * move versions.tf to main.tf file
    
    * adding more providers
    
    * bump pre-commit version
    
    * set a version module in the bug report
    
    * set `anything else` section as non mandatory for bugs and features 
    
    ---------
    
    Co-authored-by: Piotr Stawarski <p@stawarski.pl>

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
-# Terraform code goes here
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    #    aws = {
+    #      source  = "hashicorp/aws"
+    #      version = ">= 5.44.0"
+    #    }
+    #    azurerm = {
+    #      source  = "hashicorp/azurerm"
+    #      version = ">= 3.98.0"
+    #    }
+    #    digitalocean = {
+    #      source  = "digitalocean/digitalocean"
+    #      version = ">= 2.36.0"
+    #    }
+    #    github = {
+    #      source  = "integrations/github"
+    #      version = ">= 6.2.1"
+    #    }
+  }
+}
+
+# TODO: Add resources here.
