a184f2045c0f640ea850f82610ffd80f8588f66c
Michal Tomczuk <michal.tomczuk@zdt.io>
2024-04-09 18:26:26 +0200

    merge versions.tf and main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-# Terraform code goes here
+terraform {
+  required_version = ">= 1.5.5"
+  required_providers {
+    #    aws = {
+    #      source  = "hashicorp/aws"
+    #      version = ">= 5.44.0"
+    #    }
+    #    azurerm = {
+    #      source  = "hashicorp/azurerm"
+    #      version = ">= 3.98.0"
+    #    }
+    #    digitalocean = {
+    #      source  = "digitalocean/digitalocean"
+    #      version = ">= 2.36.0"
+    }
+  }
+}
