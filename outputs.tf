c87b71485fe14254883a7080258ea8ba806bcdb8
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-04-12 13:09:09 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-# output "variable" {
-#   description = "output variable description"
-#   value       = variable.main.name
-# }
+output "id" {
+  description = "A string value unique to the resource instance."
+  value       = terraform_data.main.id
+}
+
+output "full_name" {
+  description = "The full name of the resource."
+  value       = terraform_data.main.output.full_name
+}
+
+output "byte_size" {
+  description = "The size of the resource, in bytes."
+  value       = terraform_data.main.output.byte_size
+}
